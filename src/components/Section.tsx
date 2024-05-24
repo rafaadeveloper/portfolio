02139a3f8dffcef5dc7d2f3c3cec9a82f3f0ecb5
Rafael Andrade <150543648+rafaadeveloper@users.noreply.github.com>
2024-05-24 17:30:29 -0300

    Section and Picture components created

---
+++ src/components/Section.tsx
@@ -0,0 +1,11 @@
+export function Section() {
+  return (
+    <section className="flex-1 flex items-center justify-center">
+      <div className="max-w-2xl w-full flex flex-col items-start gap-8">
+        <div className="space-y-4">
+          
+        </div>
+      </div>
+    </section>
+  );
+}
