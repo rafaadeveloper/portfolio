39c374df1cc077291a015b4d0811857131495b56
Rafael Andrade <150543648+rafaadeveloper@users.noreply.github.com>
2024-05-24 17:28:29 -0300

    Header.tsx component created

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+export function Header() {
+  const links = [
+    {
+      label: "Github",
+      link: "https://github.com/rafaadeveloper",
+    },
+  ];
+
+  return (
+    <header className="ml-auto flex items-center gap-8 text-secondary">
+      {links.map((link) => (
+        <a
+          className="text-primary underline-offset-4 hover:underline"
+          href={link.link}
+        >
+          {link.label}
+        </a>
+      ))}
+    </header>
+  );
+}
