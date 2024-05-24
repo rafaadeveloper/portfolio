d054319a335babc892c2536a2b9b939c5252d7ba
Rafael Andrade <150543648+rafaadeveloper@users.noreply.github.com>
2024-05-24 18:34:40 -0300

    fix in Title.tsx

--- src/components/About/Title.tsx
+++ src/components/About/Title.tsx
@@ -8,7 +8,7 @@ export function Title() {
       <div className="flex items-start flex-col">
         <h1 className="text-lg font-semibold">Hi, I'm Rafael</h1>
         <p className="text-lg text-primary/70">
-          , but you can call me <span className="text-emerald-400">Rafa7</span>
+          but you can call me <span className="text-emerald-400">Rafa7</span>
         </p>
       </div>
     </div>
