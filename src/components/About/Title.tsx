9d34814f28bf13ecb58f0b3d2a876fccdc22fe33
Rafael Andrade <150543648+rafaadeveloper@users.noreply.github.com>
2024-05-24 18:31:38 -0300

    Made changes to the Title.tsx file, it is now responsive.

--- src/components/About/Title.tsx
+++ src/components/About/Title.tsx
@@ -5,7 +5,7 @@ export function Title() {
     <div className="space-y-4">
       <Picture />
 
-      <div className="flex items-baseline">
+      <div className="flex items-start flex-col">
         <h1 className="text-lg font-semibold">Hi, I'm Rafael</h1>
         <p className="text-lg text-primary/70">
           , but you can call me <span className="text-emerald-400">Rafa7</span>
