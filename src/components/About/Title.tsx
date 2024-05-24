21f29663b0d34425adee122d72c0c6997efbbad6
Rafael Andrade <150543648+rafaadeveloper@users.noreply.github.com>
2024-05-24 18:22:20 -0300

    Update Title.tsx

--- src/components/About/Title.tsx
+++ src/components/About/Title.tsx
@@ -4,11 +4,11 @@ export function Title() {
   return (
     <div className="space-y-4">
       <Picture />
+
       <div className="flex items-baseline">
-        <h1 className="text-lg font-bold">Hi, i'm Rafael</h1>
+        <h1 className="text-lg font-semibold">Hi, I'm Rafael</h1>
         <p className="text-lg text-primary/70">
-          , but you can call me
-          <span className="text-emerald-400 font-medium"> Rafa7</span>
+          , but you can call me <span className="text-emerald-400">Rafa7</span>
         </p>
       </div>
     </div>
