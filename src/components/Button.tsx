f24c2b7bd3cdaa6271233c5dd9ae73cad502e6eb
Rafael Andrade <150543648+rafaadeveloper@users.noreply.github.com>
2024-05-24 18:42:49 -0300

    Added function to contact button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,13 @@
 import { ComponentProps } from "react";
 
-export type ButtonProps = ComponentProps<"button">;
+export type ButtonProps = ComponentProps<"a">;
 
 export function Button({ ...props }: ButtonProps) {
   return (
-    <button
+    <a
       className="bg-primary text-background px-8 py-2 rounded-full font-semibold hover:bg-primary/80"
       {...props}
+      href="mailto:rafadevcontatos@gmail.com"
     />
   );
 }
