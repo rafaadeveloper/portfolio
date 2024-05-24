127350a661348270f5df558467efa20c8f367458
Rafael Andrade <150543648+rafaadeveloper@users.noreply.github.com>
2024-05-24 17:45:06 -0300

    Added and made the Section component

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,7 +3,13 @@ export function Section() {
     <section className="flex-1 flex items-center justify-center">
       <div className="max-w-2xl w-full flex flex-col items-start gap-8">
         <div className="space-y-4">
-          
+          <div className="flex items-baseline">
+            <h1 className="text-lg font-bold">Hi, i'm Rafael</h1>
+            <p className="text-lg text-primary/70">
+              , but you can call me
+              <span className="text-violet-400 font-medium"> Rafa7</span>
+            </p>
+          </div>
         </div>
       </div>
     </section>
