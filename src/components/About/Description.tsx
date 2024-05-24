bfd8362675453c2b4e7a93c636a2232b31290836
Rafael Andrade <150543648+rafaadeveloper@users.noreply.github.com>
2024-05-24 18:26:26 -0300

    Update Description.tsx

--- src/components/About/Description.tsx
+++ src/components/About/Description.tsx
@@ -2,8 +2,7 @@ export function Description() {
   return (
     <div className="text-secondary max-w-md">
       <p>
-        I'm a <span className="text-primary">Frontend Software Engineer</span>,
-        based in Brazil.
+        I'm a <span className="text-primary">Frontend Software Engineer</span>
       </p>
 
       <p>
