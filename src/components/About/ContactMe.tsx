f0a24e4bf36301a5c26faf9d8fb2cf4858367c32
Rafael Andrade <150543648+rafaadeveloper@users.noreply.github.com>
2024-05-26 20:23:07 -0300

    Changed contact interface

--- src/components/About/ContactMe.tsx
+++ src/components/About/ContactMe.tsx
@@ -1,5 +1,30 @@
+import { Mail, Phone, Store } from "lucide-react";
 import { Button } from "../Button";
 
 export function ContactMe() {
-  return <Button>Contact me</Button>;
+  return (
+    <div className="flex flex-col items-start gap-3">
+      <h1>Find me using:</h1>
+      <div className="flex flex-row items-start gap-3">
+        <a
+          href="https://wa.me/11990076153"
+          className="w-auto h-auto p-1.5 rounded-md flex items-center justify-center border border-solid border-white/10 hover:border-white hover:transition-all hover:duration-300"
+        >
+          <Phone size={24} />
+        </a>
+        <a
+          href="mailto:rafadevcontatos@gmail.com"
+          className="w-auto h-auto p-1.5 rounded-md flex items-center justify-center border border-solid border-white/10 hover:border-white hover:transition-all hover:duration-300"
+        >
+          <Mail size={24} />
+        </a>
+        <a
+          href="https://discord.gg/DYXYzrfgGy"
+          className="w-auto h-auto p-1.5 rounded-md flex items-center justify-center border border-solid border-white/10 hover:border-white hover:transition-all hover:duration-300"
+        >
+          <Store size={24} />
+        </a>
+      </div>
+    </div>
+  );
 }
