75a34e2800df408929d7adb6e63ec7db32a1e463
Rafael Andrade <150543648+rafaadeveloper@users.noreply.github.com>
2024-05-26 20:28:28 -0300

    Added target blank to contact links

--- src/components/About/ContactMe.tsx
+++ src/components/About/ContactMe.tsx
@@ -7,18 +7,21 @@ export function ContactMe() {
       <h1>Find me using:</h1>
       <div className="flex flex-row items-start gap-3">
         <a
+          target="_blank"
           href="https://wa.me/11990076153"
           className="w-auto h-auto p-1.5 rounded-md flex items-center justify-center border border-solid border-white/10 hover:border-white hover:transition-all hover:duration-300"
         >
           <Phone size={24} />
         </a>
         <a
+          target="_blank"
           href="mailto:rafadevcontatos@gmail.com"
           className="w-auto h-auto p-1.5 rounded-md flex items-center justify-center border border-solid border-white/10 hover:border-white hover:transition-all hover:duration-300"
         >
           <Mail size={24} />
         </a>
         <a
+          target="_blank"
           href="https://discord.gg/DYXYzrfgGy"
           className="w-auto h-auto p-1.5 rounded-md flex items-center justify-center border border-solid border-white/10 hover:border-white hover:transition-all hover:duration-300"
         >
